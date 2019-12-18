0dbf02bb661c84305c611d361ea59cb82edb49b6
weizhenming-white <weizhenming01@163.com>
2019-12-18 10:40:36 +0800

    learn c++ loops and expressions

---
+++ learn_c++/loops_and_expressions/textin2.cpp
@@ -0,0 +1,18 @@
+// textin1.cpp -- using cin.get(char)
+#include <iostream>
+int main()
+{
+    using namespace std;
+    char ch;
+    int count = 0;                  
+    cout << "Enter characters; enter # to quit: \n";
+    cin.get(ch);              // use the cin.get(ch) function
+    while (ch != '#')              
+    {
+        cout << ch;                         
+        ++count;                            
+        cin.get(ch);                          // use it again
+    }
+    cout << endl << count << " characters read\n";
+    return 0;
+}
